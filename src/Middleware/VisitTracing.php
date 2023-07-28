f981b62c757e596ec0652ff65284f37c73e0fa18
Bruno Falcão <bruno.c.falcao@brunofalcao.me>
2023-07-29 00:29:06 +0200

    Update VisitTracing.php

--- src/Middleware/VisitTracing.php
+++ src/Middleware/VisitTracing.php
@@ -7,14 +7,11 @@ use Illuminate\Http\Request;
 
 class VisitTracing
 {
-    /**
-     * Handle an incoming request.
-     *
-     * @return mixed
-     */
     public function handle(Request $request, Closure $next)
     {
-        app('tracer-visit')->record();
+        if (!app()->runningInConsole()) {
+            app('tracer-visit')->record();
+        }
 
         return $next($request);
     }
